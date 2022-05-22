dc1f8a7f1ce6e64ed2f3968592faedf95f4eec2c
Alex Thornton <athornton4@eagles.ewu.edu>
2022-05-22 14:03:12 -0700

    added test function to functionOne file

--- Functions/FunctionOne.js
+++ Functions/FunctionOne.js
@@ -3,5 +3,13 @@ import tennisData from '../tennis.json' assert {type: 'json'}
 write a function that returns the number of tournements played on grass (tourney_surface) between 1980 and 2000. 
 hint: you'll have to pull the year from tourney_dates so look into the split() method 
 */
+function tournementsOnGrassBetween1980And2000() {
+    //test
+    const names = [];
+    for(let i = 0; i < tennisData.length; i++) {
+        names.push(tennisData[i].tourney_name)
+    }
+    return names;
+}
 
-console.log("here")
+console.log(tournementsOnGrassBetween1980And2000())
